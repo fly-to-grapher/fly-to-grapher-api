bbf39f47466ae12247812f58dfddec11f822a63c
mohndkh20 <mondkaled4@gmail.com>
2022-08-05 12:22:34 +0300

    add cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var models = require('./models')
+const cors = require('cors')
 
 var usersRouter = require('./routes/users');
 var filesRouter = require('./routes/files')
@@ -23,6 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 app.use('/users', usersRouter);
 app.use('/files', filesRouter);
